normalization correct

angles.py
@@ -46,5 +46,15 @@ def theta_summary(out_mat, angle_vector):
     sum_mat = out_mat.groupby('theta_in').apply(np.sum, args=(1, None))
     sum_mat = sum_mat.groupby('theta_out').apply(np.sum, args=(0, None))
 
-    return sum_mat.data
+    R = sum_mat[sum_mat.coords['theta_out'] < np.pi / 2, :].reduce(np.sum, 'theta_out').data
+    T = sum_mat[sum_mat.coords['theta_out'] > np.pi / 2, :].reduce(np.sum, 'theta_out').data
 
+    return sum_mat.data, R, T
+
+def theta_summary_A(A_mat, angle_vector):
+    A_mat = xr.DataArray(A_mat, dims=['layer_out', 'index_in'],
+                           coords={'theta_in': (['index_in'], angle_vector[:A_mat.shape[1],1]),
+                                   'layer_out': 1+np.arange(A_mat.shape[0])})
+    sum_mat = A_mat.groupby('theta_in').apply(np.sum, args=(1, None))
+
+    return sum_mat.data

ray_tracing/rt_lookup.py
@@ -86,7 +86,7 @@ def RT(group, incidence, transmission, surf_name, n_absorbing_layers, options):
     #allArrays = [RT_wl(i1, n_angles, nx, ny, z_pos, widths, thetas_in, phis_in, h, xs, ys, nks, alphas, surfaces,
     #      I_thresh, pol, phi_sym, theta_intv, phi_intv, angle_vector) for i1 in range(len(wavelengths))]
     if options['parallel']:
-        allArrays = Parallel(n_jobs=-1)(delayed(RT_wl)
+        allres = Parallel(n_jobs=-1)(delayed(RT_wl)
                                        (i1, wavelengths[i1], n_angles, nx, ny, z_pos,
                                         widths, thetas_in, phis_in, h,
                                         xs, ys, nks, alphas, surfaces,
@@ -95,20 +95,22 @@ def RT(group, incidence, transmission, surf_name, n_absorbing_layers, options):
                                    for i1 in range(len(wavelengths)))
 
     else:
-        allArrays = [RT_wl(i1, wavelengths[i1], n_angles, nx, ny, z_pos, widths, thetas_in, phis_in, h, xs, ys, nks, alphas, surfaces,
+        allres = [RT_wl(i1, wavelengths[i1], n_angles, nx, ny, z_pos, widths, thetas_in, phis_in, h, xs, ys, nks, alphas, surfaces,
                                  I_thresh, pol, phi_sym, theta_intv, phi_intv, angle_vector, Fr_or_TMM, n_absorbing_layers) for i1 in range(len(wavelengths))]
 
-    allArrays = stack(allArrays)
+    allArrays = stack([item[0] for item in allres])
+    absArrays = np.stack([item[1] for item in allres])
     structpath = os.path.join(results_path, options['struct_name'])
     if not os.path.isdir(structpath):
         os.mkdir(structpath)
     savepath = os.path.join(structpath, surf_name + '.npz')
     save_npz(savepath, allArrays)
-    return allArrays
+    return allArrays, absArrays
 
 
 def RT_wl(i1, wl, n_angles, nx, ny, z_pos, widths, thetas_in, phis_in, h, xs, ys, nks, alphas, surfaces,
           I_thresh, pol, phi_sym, theta_intv, phi_intv, angle_vector, Fr_or_TMM, n_abs_layers):
+    print('wavelength = ', wl)
 
     theta_out = np.zeros((n_angles, nx * ny))
     phi_out = np.zeros((n_angles, nx * ny))
@@ -131,6 +133,7 @@ def RT_wl(i1, wl, n_angles, nx, ny, z_pos, widths, thetas_in, phis_in, h, xs, ys
             phi_out[i2, c] = phi_o
             A_layer[i2] = A_layer[i2] + A_per_layer / (nx * ny)
 
+    print('done ray-tracing, binning')
     phi_out = fold_phi(phi_out, phi_sym)
     phis_in = fold_phi(phis_in, phi_sym)
 
@@ -145,7 +148,6 @@ def RT_wl(i1, wl, n_angles, nx, ny, z_pos, widths, thetas_in, phis_in, h, xs, ys
     # xarray: can use coordinates in calculations using apply!
     binned_theta_in = np.digitize(thetas_in, theta_intv, right=True) - 1
     binned_theta_out = np.digitize(theta_out, theta_intv, right=True) - 1
-    print('binned theta, len angle vec', binned_theta_out, len(angle_vector))
     # -1 to give the correct index for the bins in phi_intv
 
     phi_in = xr.DataArray(phis_in,
@@ -166,21 +168,27 @@ def RT_wl(i1, wl, n_angles, nx, ny, z_pos, widths, thetas_in, phis_in, h, xs, ys
     # everything is coming in from above so we don't need 90 -> 180 in incoming bins
     A_mat = np.zeros((n_abs_layers, int(len(angle_vector)/2)))
 
-    for l1, theta_in in enumerate(thetas_in):
+    n_abs = np.zeros((1, int(len(angle_vector)/2)))
+    n_notabs = np.zeros((1, int(len(angle_vector)/2)))
+
+    for l1 in range(len(thetas_in)):
         for l2 in range(nx * ny):
             if binned_theta_out[l1, l2] <= (n_thetas-1):
                 out_mat[bin_out[l1, l2], bin_in[l1]] += 1
+
             else:
                 A_mat[binned_theta_out[l1, l2]-n_thetas, bin_in[l1]] += 1
 
-        # fraction absorbed
-        out_mat[:, bin_in[l1]] = out_mat[:, bin_in[l1]] / np.sum(out_mat[:, bin_in[l1]])
+        #out_mat[:, bin_in[l1]] = out_mat[:, bin_in[l1]] / np.sum(out_mat[:, bin_in[l1]])
+    total_rays = np.sum(out_mat) + np.sum(A_mat)
+    out_mat = out_mat/total_rays
+    A_mat = A_mat/total_rays
 
     result = COO(out_mat)  # sparse matrix
 
     #savepath = os.path.join(structpath, surf_name + '_' + "{0:.6g}".format(wl * 1e9) + '.npz')
     #save_npz(savepath, result)
-    return result
+    return result, A_mat
 
 def normalize(x):
     if sum(x > 0):

test_RTlookup.py
@@ -1,5 +1,3 @@
-from multiprocessing import freeze_support
-
 from solcore import material
 from solcore import si
 
@@ -36,7 +34,7 @@ surf_back = RTSurface(Points)
 wavelengths = np.linspace(900, 1160, 2)*1e-9
 #pool = Pool(processes = 4)
 options =  {'wavelengths': wavelengths, 'I_thresh': 1e-4, 'theta': 0, 'phi': 0,
-            'nx': 2, 'ny': 2, 'max_passes': 100, 'parallel': False, 'n_rays': 1000,
+            'nx': 2, 'ny': 2, 'max_passes': 100, 'parallel': False, 'n_rays': 2000,
             'phi_symmetry': np.pi/2, 'n_theta_bins': 50, 'c_azimuth': 0.25,
             'random_angles': False, 'pol': 's', 'struct_name': 'testing', 'Fr_or_TMM': 1}#,
             #'pool': pool}
@@ -47,22 +45,29 @@ surf = Surface('RT', None, texture = surf, depth_spacing = si('1nm'))
 start = time()
 group = RTgroup(textures=[surf.texture])
 
-incidence = Si
-transmission = Air
+incidence = Air
+transmission = Si
 
-allArrays = RT(group, incidence, transmission, 'GeGaAsstack', 2, options)
+allArrays, absArrays = RT(group, incidence, transmission, 'GeGaAsstack', 2, options)
 print('Time taken = ', time() - start, ' s')
 
-out_mat = allArrays[1]
+out_mat = allArrays[0]
 outfull = out_mat.todense()
 
-from angles import theta_summary, make_angle_vector
+from angles import theta_summary, theta_summary_A, make_angle_vector
 
 theta_intv, phi_intv, angle_vector = make_angle_vector(options['n_theta_bins'],
                                                        options['phi_symmetry'],
                                                        options['c_azimuth'])
 
-theta_sum = theta_summary(outfull, angle_vector)
+theta_sum, R, T = theta_summary(outfull, angle_vector)
 
 plt.imshow(theta_sum, cmap='hot', interpolation='nearest')
 plt.show()
+
+A_sum = theta_summary_A(absArrays[0], angle_vector)
+thetas = (theta_intv[1:]+theta_intv[:-1])/2
+thetas = thetas[:int(len(thetas)/2)]
+plt.figure()
+plt.plot(thetas, A_sum[0])
+plt.plot(thetas, A_sum[1])