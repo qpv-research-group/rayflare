setting up matrix multiplication. corrected matrix normalization

ray_tracing/rt_lookup.py
@@ -14,12 +14,11 @@ from sparse import COO, save_npz, stack
 from config import results_path
 from joblib import Parallel, delayed
 
-global lookuptable
 # want a Delaunay-type triangulation object which has the same attributes as
 # the return from scipy.spatial.Delaunaytheta_t = np.asin((n1/n2)*np.sin(theta)), but contains 3D rather than 2D coordinates,
 # i.e. a 2D surface in 3D space.
 
-def RT(group, incidence, transmission, surf_name, n_absorbing_layers, options):
+def RT(group, incidence, transmission, surf_name, n_absorbing_layers, options, front_or_rear='front'):
 
     structpath = os.path.join(results_path, options['struct_name'])
     if not os.path.isdir(structpath):
@@ -38,8 +37,11 @@ def RT(group, incidence, transmission, surf_name, n_absorbing_layers, options):
     Fr_or_TMM = options['Fr_or_TMM']
 
     if Fr_or_TMM == 1:
-        global lookuptable
         lookuptable = xr.open_dataset(os.path.join(structpath, surf_name + '.nc'))
+        if front_or_rear == 'rear':
+            lookuptable = lookuptable.assign_coords(side=np.flip(allres.side))
+    else:
+        lookuptable = None
 
     theta_intv, phi_intv, angle_vector = make_angle_vector(n_theta_bins, phi_sym, c_az)
 
@@ -47,7 +49,7 @@ def RT(group, incidence, transmission, surf_name, n_absorbing_layers, options):
         thetas_in = np.random.random(n_angles)*np.pi/2
         phis_in = np.random.random(n_angles)*2*np.pi
     else:
-        angles_in = angle_vector[1:int(len(angle_vector)/2),:]
+        angles_in = angle_vector[:int(len(angle_vector)/2),:]
         n_reps = int(np.ceil(n_angles/len(angles_in)))
         thetas_in = np.tile(angles_in[:,1], n_reps)[:n_angles]
         phis_in = np.tile(angles_in[:,2], n_reps)[:n_angles]
@@ -91,25 +93,31 @@ def RT(group, incidence, transmission, surf_name, n_absorbing_layers, options):
                                         widths, thetas_in, phis_in, h,
                                         xs, ys, nks, alphas, surfaces,
                                         I_thresh, pol, phi_sym, theta_intv,
-                                        phi_intv, angle_vector, Fr_or_TMM, n_absorbing_layers)
+                                        phi_intv, angle_vector, Fr_or_TMM, n_absorbing_layers,
+                                        lookuptable)
                                    for i1 in range(len(wavelengths)))
 
     else:
-        allres = [RT_wl(i1, wavelengths[i1], n_angles, nx, ny, z_pos, widths, thetas_in, phis_in, h, xs, ys, nks, alphas, surfaces,
-                                 I_thresh, pol, phi_sym, theta_intv, phi_intv, angle_vector, Fr_or_TMM, n_absorbing_layers) for i1 in range(len(wavelengths))]
+        allres = [RT_wl(i1, wavelengths[i1], n_angles, nx, ny, z_pos, widths,
+                        thetas_in, phis_in, h, xs, ys, nks, alphas, surfaces,
+                                 I_thresh, pol, phi_sym, theta_intv, phi_intv,
+                        angle_vector, Fr_or_TMM, n_absorbing_layers, lookuptable)
+                  for i1 in range(len(wavelengths))]
 
     allArrays = stack([item[0] for item in allres])
-    absArrays = np.stack([item[1] for item in allres])
+    absArrays = stack([item[1] for item in allres])
     structpath = os.path.join(results_path, options['struct_name'])
     if not os.path.isdir(structpath):
         os.mkdir(structpath)
-    savepath = os.path.join(structpath, surf_name + '.npz')
-    save_npz(savepath, allArrays)
+    savepath_RT = os.path.join(structpath, surf_name + 'RT.npz')
+    savepath_A = os.path.join(structpath, surf_name + 'A.npz')
+    save_npz(savepath_RT, allArrays)
+    save_npz(savepath_A, absArrays)
     return allArrays, absArrays
 
 
 def RT_wl(i1, wl, n_angles, nx, ny, z_pos, widths, thetas_in, phis_in, h, xs, ys, nks, alphas, surfaces,
-          I_thresh, pol, phi_sym, theta_intv, phi_intv, angle_vector, Fr_or_TMM, n_abs_layers):
+          I_thresh, pol, phi_sym, theta_intv, phi_intv, angle_vector, Fr_or_TMM, n_abs_layers, lookuptable):
     print('wavelength = ', wl)
 
     theta_out = np.zeros((n_angles, nx * ny))
@@ -127,7 +135,8 @@ def RT_wl(i1, wl, n_angles, nx, ny, z_pos, widths, thetas_in, phis_in, h, xs, ys
         for c, vals in enumerate(product(xs, ys)):
             I, profile, A_per_layer, th_o, phi_o, ray_path = \
                 single_ray(vals[0], vals[1], nks[:, i1], alphas[:, i1],
-                           r_a_0, theta, phi, surfaces, widths, z_pos, I_thresh, pol, wl, Fr_or_TMM)
+                           r_a_0, theta, phi, surfaces, widths, z_pos,
+                           I_thresh, pol, wl, Fr_or_TMM, lookuptable)
             absorption_profiles[i2] = absorption_profiles[i2] + profile / (nx * ny)
             theta_out[i2, c] = th_o
             phi_out[i2, c] = phi_o
@@ -147,6 +156,7 @@ def RT_wl(i1, wl, n_angles, nx, ny, z_pos, widths, thetas_in, phis_in, h, xs, ys
 
     # xarray: can use coordinates in calculations using apply!
     binned_theta_in = np.digitize(thetas_in, theta_intv, right=True) - 1
+
     binned_theta_out = np.digitize(theta_out, theta_intv, right=True) - 1
     # -1 to give the correct index for the bins in phi_intv
 
@@ -173,6 +183,7 @@ def RT_wl(i1, wl, n_angles, nx, ny, z_pos, widths, thetas_in, phis_in, h, xs, ys
 
     for l1 in range(len(thetas_in)):
         for l2 in range(nx * ny):
+            print(thetas_in[l1], bin_in[l1], bin_out[l1,l2])
             if binned_theta_out[l1, l2] <= (n_thetas-1):
                 out_mat[bin_out[l1, l2], bin_in[l1]] += 1
 
@@ -180,15 +191,18 @@ def RT_wl(i1, wl, n_angles, nx, ny, z_pos, widths, thetas_in, phis_in, h, xs, ys
                 A_mat[binned_theta_out[l1, l2]-n_thetas, bin_in[l1]] += 1
 
         #out_mat[:, bin_in[l1]] = out_mat[:, bin_in[l1]] / np.sum(out_mat[:, bin_in[l1]])
-    total_rays = np.sum(out_mat) + np.sum(A_mat)
-    out_mat = out_mat/total_rays
-    A_mat = A_mat/total_rays
 
-    result = COO(out_mat)  # sparse matrix
+    # normalize
+    norm_factor = np.sum(out_mat, axis = 0) + np.sum(A_mat, axis = 0)
+    out_mat = out_mat/norm_factor
+    A_mat = A_mat/norm_factor
 
+
+    out_mat = COO(out_mat)  # sparse matrix
+    A_mat = COO(A_mat)
     #savepath = os.path.join(structpath, surf_name + '_' + "{0:.6g}".format(wl * 1e9) + '.npz')
     #save_npz(savepath, result)
-    return result, A_mat
+    return out_mat, A_mat
 
 def normalize(x):
     if sum(x > 0):
@@ -258,7 +272,7 @@ def calc_angle(x):
     v1 = np.array([0, 1])
     return np.math.atan2(np.linalg.det([x, v1]), np.dot(x, v1))  # - 180 to 180
 
-def single_ray(x, y,  nks, alphas, r_a_0, theta, phi, surfaces, widths, z_pos, I_thresh, pol, wl, Fr_or_TMM):
+def single_ray(x, y,  nks, alphas, r_a_0, theta, phi, surfaces, widths, z_pos, I_thresh, pol, wl, Fr_or_TMM, lookuptable):
     # final_res = 0: reflection
     # final_res = 1: transmission
     # This should get a list of surfaces and materials (optical constants, alpha + widths); there is one less surface than material
@@ -314,7 +328,8 @@ def single_ray(x, y,  nks, alphas, r_a_0, theta, phi, surfaces, widths, z_pos, I
         ray_path = np.vstack((ray_path, np.append(r_a + d*(((surf.zcov - r_a[2])/d[2])-2), 1)))
         #print(r_a, d)
         res, theta, phi, r_a, d, ray_path = single_interface_check(r_a, d, nks[mat_index],
-                                     nks[mat_index+1], surf, surf.Lx, surf.Ly, direction, surf.zcov, ray_path, pol, wl, Fr_or_TMM)
+                                     nks[mat_index+1], surf, surf.Lx, surf.Ly, direction,
+                                     surf.zcov, ray_path, pol, wl, Fr_or_TMM, lookuptable)
 
         if res == 0:  # reflection
             direction = -direction # changing direction due to reflection
@@ -387,7 +402,7 @@ def traverse(width, theta, alpha, x, y, I_i, positions, I_thresh, direction):
     return DA, stop, I_back, theta
 
 
-def decide_RT_Fresnel(n0, n1, theta, d, N, side, pol, rnd, wl):
+def decide_RT_Fresnel(n0, n1, theta, d, N, side, pol, rnd, wl = None, lookuptable = None):
     R = calc_R(n0, n1, theta, pol)
 
     if rnd <= R:  # REFLECTION
@@ -409,8 +424,9 @@ def decide_RT_Fresnel(n0, n1, theta, d, N, side, pol, rnd, wl):
 
     return d, side, False # never absorbed, A = False
 
-def decide_RT_TMM(n0, n1, theta, d, N, side, pol, rnd, wl):
-    data = lookuptable.loc[dict(side=side, pol=pol)].interp(wl=wl*1e9, angle=abs(theta))
+def decide_RT_TMM(n0, n1, theta, d, N, side, pol, rnd, wl, lookuptable):
+    #data = lookuptable.loc[dict(side=side, pol=pol)].interp(wl=wl*1e9, angle=abs(theta))
+    data = lookuptable.loc[dict(side=side, pol=pol)].sel(angle=abs(theta), wl=wl*1e9, method='nearest')
     R = np.real(data['R'].data.item(0))
     T = np.real(data['T'].data.item(0))
     A_per_layer = np.real(data['Alayer'].data)
@@ -446,7 +462,7 @@ def decide_RT_TMM(n0, n1, theta, d, N, side, pol, rnd, wl):
     return d, side, A
 
 
-def single_interface_check(r_a, d, ni, nj, tri, Lx, Ly, side, z_cov, ray_path, pol, wl, Fr_or_TMM):
+def single_interface_check(r_a, d, ni, nj, tri, Lx, Ly, side, z_cov, ray_path, pol, wl, Fr_or_TMM, lookuptable):
 
     decide = {0: decide_RT_Fresnel, 1: decide_RT_TMM}
 
@@ -520,7 +536,7 @@ def single_interface_check(r_a, d, ni, nj, tri, Lx, Ly, side, z_cov, ray_path, p
 
             rnd = random()
 
-            d, side, A = decide[Fr_or_TMM](n0, n1, theta, d, N, side, pol, rnd, wl)
+            d, side, A = decide[Fr_or_TMM](n0, n1, theta, d, N, side, pol, rnd, wl, lookuptable)
 
             r_a = np.real(intersn + d / 1e9) # this is to make sure the raytracer doesn't immediately just find the same intersection again
 

test_RTlookup.py
@@ -31,11 +31,11 @@ z = np.array([0, 0, 0, 0])
 
 Points = np.vstack([x, y, z]).T
 surf_back = RTSurface(Points)
-wavelengths = np.linspace(900, 1160, 2)*1e-9
+wavelengths = np.linspace(700, 800, 2)*1e-9
 #pool = Pool(processes = 4)
 options =  {'wavelengths': wavelengths, 'I_thresh': 1e-4, 'theta': 0, 'phi': 0,
-            'nx': 2, 'ny': 2, 'max_passes': 100, 'parallel': False, 'n_rays': 2000,
-            'phi_symmetry': np.pi/2, 'n_theta_bins': 50, 'c_azimuth': 0.25,
+            'nx': 2, 'ny': 2, 'max_passes': 100, 'parallel': True, 'n_rays': 10000,
+            'phi_symmetry': np.pi/2, 'n_theta_bins': 100, 'c_azimuth': 0.25,
             'random_angles': False, 'pol': 's', 'struct_name': 'testing', 'Fr_or_TMM': 1}#,
             #'pool': pool}
 
@@ -53,6 +53,7 @@ print('Time taken = ', time() - start, ' s')
 
 out_mat = allArrays[0]
 outfull = out_mat.todense()
+absArrays = absArrays.todense()
 
 from angles import theta_summary, theta_summary_A, make_angle_vector
 

transfer_matrix_method/lookup_table.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import os
 from config import results_path
 
-def make_TMM_lookuptable(layers, substrate, incidence, surf_name, options):
+def make_TMM_lookuptable(layers, substrate, incidence, surf_name, options, profile=False, prof_layers=None):
 
     wavelengths = options['wavelengths']
     #pol = options['pol']
@@ -22,7 +22,7 @@ def make_TMM_lookuptable(layers, substrate, incidence, surf_name, options):
     optstacks = [optlayers, optlayers_flip]
 
     if coherency_list is not None:
-        coherency_lists = [coherency_list, coherency_list[::,-1]]
+        coherency_lists = [coherency_list, coherency_list[::-1]]
     else:
         coherency_lists = [None, None]
     # can calculate by angle, already vectorized over wavelength
@@ -45,20 +45,33 @@ def make_TMM_lookuptable(layers, substrate, incidence, surf_name, options):
                                   'angle': thetas,
                                   'layer': range(1, n_layers + 1)}, name='Alayer')
 
+    if profile:
+        Aprof = xr.DataArray(np.empty((2, 2, n_angles, 5, len(prof_layers), len(wavelengths))),
+                              dims=['side', 'pol', 'angle', 'coeff', 'layer', 'wl'],
+                          coords={'side': [1, -1], 'pol': pols,
+                                  'wl': wavelengths,
+                                  'angle': thetas,
+                                  'layer': prof_layers,
+                                  'coeff': ['A1', 'A2', 'A3', 'a1', 'a3']}, name='Aprof')
+
+
     for i1, side in enumerate(sides):
         R_loop = np.empty((len(wavelengths), n_angles))
         T_loop = np.empty((len(wavelengths), n_angles))
         Alayer_loop = np.empty((n_angles, len(wavelengths), n_layers))
+        if profile:
+            Aprof_loop = np.empty((n_angles, 5, len(prof_layers), len(wavelengths)))
 
         for i2, pol in enumerate(pols):
 
             for i3, theta in enumerate(thetas):
                 res = calculate_rat(optstacks[i1], wavelengths, angle=theta, pol=pol,
-                                coherent=coherent, coherency_list=coherency_lists[i1],
-                                no_back_reflection=False)
+                                coherent=coherent, coherency_list=coherency_lists[i1], profile=profile, layers=prof_layers)
                 R_loop[:, i3] = np.real(res['R'])
                 T_loop[:, i3] = np.real(res['T'])
                 Alayer_loop[i3, :, :] = np.real(res['A_per_layer'].T)
+                if profile:
+                    Aprof_loop[i3, :, :, :] = res['profile_coeff']
 
             # sometimes get very small negative values (like -1e-20)
             R_loop[R_loop<0] = 0
@@ -67,13 +80,24 @@ def make_TMM_lookuptable(layers, substrate, incidence, surf_name, options):
 
             if side == -1:
                 Alayer_loop = np.flip(Alayer_loop, axis = 2)
+                if profile:
+                    Aprof_loop = np.flip(Aprof_loop, axis = 2)
 
             R.loc[dict(side=side, pol=pol)] = R_loop
             T.loc[dict(side=side, pol=pol)] = T_loop
             Alayer.loc[dict(side=side, pol=pol)] = Alayer_loop
 
+            if profile:
+                Aprof.loc[dict(side=side, pol=pol)] = Aprof_loop
+                Aprof.transpose('side', 'pol', 'wl', 'angle', 'layer', 'coeff')
+
     Alayer = Alayer.transpose('side', 'pol', 'wl', 'angle', 'layer')
-    allres = xr.merge([R, T, Alayer])
+
+    if profile:
+        allres = xr.merge([R, T, Alayer, Aprof])
+    else:
+        allres = xr.merge([R, T, Alayer])
+
     unpol = allres.reduce(np.mean, 'pol').assign_coords(pol='u').expand_dims('pol')
     allres = allres.merge(unpol)
     structpath = os.path.join(results_path, options['struct_name'])

transfer_matrix_method/test_lookuptable.py
@@ -5,29 +5,32 @@ from solcore.structure import Layer
 from transfer_matrix_method.transfer_matrix import calculate_rat, OptiStack
 import matplotlib.pyplot as plt
 from transfer_matrix_method.lookup_table import make_TMM_lookuptable
+from transfer_matrix_method.tmm_core_vec import absorp_analytic_fn
 
 wavelengths = np.linspace(400, 1200, 150)
 options =  {'wavelengths': wavelengths, 'I_thresh': 1e-4,
             'nx': 5, 'ny': 5, 'max_passes': 100, 'parallel': False, 'n_rays': 40000,
             'phi_symmetry': np.pi/2, 'n_theta_bins': 100, 'c_azimuth': 0.25,
-            'random_angles': True, 'lookuptable_angles': 200, 'pol': 'u',
-            'coherent': True, 'coherency_list': None, 'struct_name': 'testing'}#,
+            'random_angles': True, 'lookuptable_angles': 200, 'pol': 's',
+            'coherent': False, 'coherency_list': ['c', 'c'], 'struct_name': 'testing'}#,
 
 Ge = material('Ge')()
 GaAs = material('GaAs')()
 Air = material('Air')()
+Si = material('Si')()
 
-
-layers = [Layer(100e-9, Ge), Layer(50e-9, GaAs)]
+layers = [Layer(100e-9, GaAs), Layer(500e-9, Ge)]
 
 n_layers = len(layers)
 
-substrate = Ge
+substrate = Si
 incidence = Air
 
-surf_name = 'GeGaAsstack'
+surf_name = 'GaAsGestack_prof'
 
-allres = make_TMM_lookuptable(layers, substrate, incidence, surf_name, options)
+prof_layers = [1,2]
+
+allres = make_TMM_lookuptable(layers, substrate, incidence, surf_name, options, profile=True, prof_layers=prof_layers)
 
 #unpol = allres.reduce(np.mean, 'pol').assign_coords(pol='u').expand_dims('pol')
 
@@ -54,3 +57,67 @@ for side in [1, -1]:
         allres['Alayer'].sel(side=side, pol = 'u', layer=i1).plot.imshow('wl', 'angle')
     plt.show()
 
+angles = np.arange(0, options['lookuptable_angles'], 25)
+
+#wavelengths in nm so alpha should be in nm^-1
+a1 = allres.loc[dict(side=1, wl=wavelengths[70], pol = 'u', coeff = 'a1', layer=2)].Aprof[angles].data
+A2 = allres.loc[dict(side=1, wl=wavelengths[70], pol = 'u', coeff = 'A2', layer=2)].Aprof[angles].data
+a3 = allres.loc[dict(side=1, wl=wavelengths[70], pol = 'u', coeff = 'a3', layer=2)].Aprof[angles].data
+A1 = allres.loc[dict(side=1, wl=wavelengths[70], pol = 'u', coeff = 'A1', layer=2)].Aprof[angles].data
+A3 = allres.loc[dict(side=1, wl=wavelengths[70], pol = 'u', coeff = 'A3', layer=2)].Aprof[angles].data
+
+R = allres.loc[dict(side=1, wl=wavelengths[70], pol = 'u')].R[angles]
+T = allres.loc[dict(side=1, wl=wavelengths[70], pol = 'u')].T[angles]
+A = allres.loc[dict(side=1, wl=wavelengths[70], pol = 'u')].Alayer[angles]
+
+depth=np.linspace(0,500,500)
+
+a_z = absorp_analytic_fn().set(A1, A2, A3, a1, a3).run(depth)
+
+plt.figure()
+plt.plot(a_z.T)
+plt.legend(angles)
+
+
+plt.figure()
+plt.plot(angles, R)
+plt.plot(angles, A[:,0])
+plt.plot(angles, A[:,1])
+plt.legend(['R', 'A1', 'A2'])
+
+A2/a1
+
+# import xarray as xr
+# import numpy as np
+# from solcore import material
+# from solcore.structure import Layer
+# from transfer_matrix_method.transfer_matrix import calculate_rat, OptiStack
+# import matplotlib.pyplot as plt
+# from transfer_matrix_method.lookup_table import make_TMM_lookuptable
+#
+# wavelengths = np.linspace(400, 1200, 10)
+# options =  {'wavelengths': wavelengths, 'I_thresh': 1e-4,
+#             'nx': 5, 'ny': 5, 'max_passes': 100, 'parallel': False, 'n_rays': 40000,
+#             'phi_symmetry': np.pi/2, 'n_theta_bins': 100, 'c_azimuth': 0.25,
+#             'random_angles': True, 'lookuptable_angles': 200, 'pol': 's',
+#             'coherent': True, 'coherency_list': None, 'struct_name': 'testing'}#,
+#
+# Ge = material('Ge')()
+# GaAs = material('GaAs')()
+# Air = material('Air')()
+#
+#
+# layers = [Layer(100e-9, Ge), Layer(50e-9, GaAs)]
+#
+#
+# substrate = Ge
+# incidence = Air
+#
+# a = OptiStack(layers, substrate=substrate, incidence=incidence)
+#
+# layer_indices = [1,2]
+#
+# coherent = False
+# coherency_list = ['c', 'i']
+#
+# output = calculate_rat(a, wavelengths, 0.5, 'u', coherent, coherency_list, True, layer_indices)

transfer_matrix_method/tmm_core_vec.py
@@ -597,6 +597,14 @@ class absorp_analytic_fn:
         self.A3 += b.A3
         return self
 
+    def set(self, A1, A2, A3, a1, a3):
+        self.A1 = A1
+        self.A2 = A2
+        self.A3 = A3
+        self.a1 = a1
+        self.a3 = a3
+        return self
+
 
 def absorp_in_each_layer(coh_tmm_data):
     """
@@ -899,7 +907,7 @@ def inc_tmm(pol, n_list, d_list, c_list, th_0, lam_vac):
         L_list.append(L)
         Ltilde = np.matmul(Ltilde, L)
 
-    print('Ltilde', Ltilde)
+    #print('Ltilde', Ltilde)
     T = 1 / Ltilde[:, 0, 0]
     R = Ltilde[:, 1, 0] / Ltilde[:, 0, 0]
 
@@ -1052,13 +1060,13 @@ def inc_position_resolved(layer, dist, inc_tmm_data, coherency_list, widths, alp
     A_local = np.zeros((len(alphas[0]), len(dist)))
     for i, l in enumerate(layers):
         if coherency_list[l] == 'c':
-            print(l, 'c')
+            #print(l, 'c')
             fn = inc_find_absorp_analytic_fn(l, inc_tmm_data)
             A_local[:, layer == l] = fn.run(dist[layer == l])
 
             #print('c', A_local[:, layer == l])
         else:
-            print(l, 'i')
+            #print(l, 'i')
             A_local[:, layer == l] = beer_lambert(widths[l]*1e-9, alphas[l]*1e9, fraction_reaching[i], dist[layer == l]*1e-9)
             #print('i', A_local[:, layer == l])
 
@@ -1087,4 +1095,3 @@ def beer_lambert(width, alphas, fraction, dist):
     # not sure why there's a nm factor (1e9) wrong here... What are internal units used by Solcore?
     # this gives correct results, in any case.
     return output/1e9
-

transfer_matrix_method/transfer_matrix.py
@@ -284,7 +284,7 @@ class OptiStack(object):
 
 
 def calculate_rat(stack, wavelength, angle=0, pol='u',
-                  coherent=True, coherency_list=None, no_back_reflection=True):
+                  coherent=True, coherency_list=None, profile=False, layers=None):
     """ Calculates the reflected, absorbed and transmitted intensity of the structure for the wavelengths and angles
     defined.
 
@@ -328,7 +328,7 @@ def calculate_rat(stack, wavelength, angle=0, pol='u',
             output['A_per_layer'] = A_per_layer[1:-1]
         else:
             out = tmm.inc_tmm(pol, stack.get_indices(wavelength), stack.get_widths(), coherency_list, angle, wavelength)
-            A_per_layer = tmm.inc_absorp_in_each_layer(out)
+            A_per_layer = np.array(tmm.inc_absorp_in_each_layer(out))
             output['R'] = out['R']
             output['A'] = 1 - out['R'] - out['T']
             output['T'] = out['T']
@@ -357,6 +357,76 @@ def calculate_rat(stack, wavelength, angle=0, pol='u',
             output['all_p'] = out_p['power_entering_list']
             output['all_s'] = out_s['power_entering_list']
             output['A_per_layer'] = 0.5*(A_per_layer_p[1:-1] + A_per_layer_s[1:-1])
+
+
+    # if requested, calculate absorption profile as well
+
+    # layer indices: 0 is incidence, n is transmission medium
+    if profile:
+        if pol in 'sp':
+            # print(stack.get_indices(wavelength).shape)
+            if coherent:
+                fn = tmm.absorp_analytic_fn().fill_in(out, layers)
+
+            else:
+                fraction_reaching = 1 - np.cumsum(A_per_layer, axis=0)
+                fn = tmm.absorp_analytic_fn()
+                fn.a1, fn.a3, fn.A1, fn.A2, fn.A3 = np.empty((0, num_wl)), np.empty((0, num_wl)), np.empty((0, num_wl)), \
+                                                    np.empty((0, num_wl)), np.empty((0, num_wl))
+
+                for l in layers:
+                    if coherency_list[l] == 'c':
+                        fn_l = tmm.inc_find_absorp_analytic_fn(l, out)
+                        fn.a1 = np.vstack((fn.a1, fn_l.a1))
+                        fn.a3 = np.vstack((fn.a3, fn_l.a3))
+                        fn.A1 = np.vstack((fn.A1, fn_l.A1))
+                        fn.A2 = np.vstack((fn.A2, fn_l.A2))
+                        fn.A3 = np.vstack((fn.A3, fn_l.A3))
+
+                    else:
+                        # DO NOT KNOW IF UNITS ARE CORRECT
+                        alpha = np.imag(stack.get_indices(wavelength)[l])*4*np.pi/wavelength
+                        fn.a1 = np.vstack((fn.a1, alpha))
+                        fn.A2 = np.vstack((fn.A2, alpha*fraction_reaching[l-1]))
+                        fn.a3 = np.vstack((fn.a3, np.zeros((1, num_wl))))
+                        fn.A1 = np.vstack((fn.A1, np.zeros((1, num_wl))))
+                        fn.A3 = np.vstack((fn.A3, np.zeros((1, num_wl))))
+
+        else:
+            if coherent:
+                fn_s = tmm.absorp_analytic_fn().fill_in(out_s, layers)
+                fn_p = tmm.absorp_analytic_fn().fill_in(out_p, layers)
+                fn = fn_s.add(fn_p).scale(0.5)
+            else:
+                fraction_reaching_s = 1 - np.cumsum(A_per_layer_s, axis=0)
+                fraction_reaching_p = 1 - np.cumsum(A_per_layer_s, axis=0)
+                fraction_reaching = 0.5*(fraction_reaching_s + fraction_reaching_p)
+                fn = tmm.absorp_analytic_fn()
+                fn.a1, fn.a3, fn.A1, fn.A2, fn.A3 = np.empty((0, num_wl)), np.empty((0, num_wl)), np.empty((0, num_wl)), \
+                                                    np.empty((0, num_wl)), np.empty((0, num_wl))
+
+                for l in layers:
+                    if coherency_list[l] == 'c':
+                        fn_s = tmm.inc_find_absorp_analytic_fn(l, out_s)
+                        fn_p = tmm.inc_find_absorp_analytic_fn(l, out_s)
+                        fn_l = fn_s.add(fn_p).scale(0.5)
+                        fn.a1 = np.vstack((fn.a1, fn_l.a1))
+                        fn.a3 = np.vstack((fn.a3, fn_l.a3))
+                        fn.A1 = np.vstack((fn.A1, fn_l.A1))
+                        fn.A2 = np.vstack((fn.A2, fn_l.A2))
+                        fn.A3 = np.vstack((fn.A3, fn_l.A3))
+
+
+                    else:
+                        # DO NOT KNOW IF UNITS ARE CORRECT
+                        alpha = np.imag(stack.get_indices(wavelength)[l]) * 4 * np.pi / wavelength
+                        fn.a1 = np.vstack((fn.a1, alpha))
+                        fn.A2 = np.vstack((fn.A2, alpha * fraction_reaching[l - 1]))
+                        fn.a3 = np.vstack((fn.a3, np.zeros((1, num_wl))))
+                        fn.A1 = np.vstack((fn.A1, np.zeros((1, num_wl))))
+                        fn.A3 = np.vstack((fn.A3, np.zeros((1, num_wl))))
+
+        output['profile_coeff'] = np.stack((fn.A1, fn.A2, fn.A3, fn.a1, fn.a3)) # shape is (5, n_layers, num_wl)
 
     return output
 
@@ -457,8 +527,6 @@ def calculate_absorption_profile(structure, wavelength, z_limit=None, steps_size
     Integrating this absorption profile in the whole stack gives the same result that the absorption obtained with
     calculate_rat as long as the spacial mesh (controlled by steps_thinest_layer) is fine enough. If the structure is
     very thick and the mesh not thin enough, the calculation might diverege at short wavelengths.
-
-    For now, it only works for normal incident, coherent light.
 
     :param structure: A solcore structure with layers and materials.
     :param wavelength: Wavelengths in which calculate the data (in nm). An array