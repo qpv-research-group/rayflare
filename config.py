bd8464f964145925f407ceda04334b2820ed04e9
phoebe-p <phoebe.pearce15@imperial.ac.uk>
2019-06-22 13:05:31 +0100

    updated config file

--- config.py
+++ config.py
@@ -1,1 +1,4 @@
-results_path = 'C:\\Users\\pmpea\\Box Sync\\RayFlare\\results'
+import os
+
+# by default, results will be stored in the RayFlare directory in a separate folder called 'results'
+results_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'results')
