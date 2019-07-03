5f29fde0de5f73f7743060f9b48d467e7c8b23a8
phoebe-p <phoebe.pearce15@imperial.ac.uk>
2019-07-03 12:36:13 +0100

    RCWA example update

--- examples/rcwa_test_matrix.py
+++ examples/rcwa_test_matrix.py
@@ -6,7 +6,7 @@ from solcore.structure import Junction, Layer
 from solcore.solar_cell import SolarCell
 from solcore.solar_cell_solver import solar_cell_solver, default_options
 from solcore.light_source import LightSource
-from rigorous_coupled_wave_analysis.rcwa import calculate_rat_rcwa
+from rigorous_coupled_wave_analysis.rcwa import rcwa
 from solcore.absorption_calculator.nk_db import download_db, search_db
 from solcore.material_system.create_new_material import create_new_material
 import xarray as xr
@@ -27,14 +27,13 @@ Si = material('Si')()
 #x = 500
 x=1000
 # anti-reflection coating
-ARC = [Layer(si('78nm'), material = MgF2), Layer(si('48nm'), material = Ta2O5)]
 
 size = ((x, 0),(0,x))
 # The layer with nanoparticles
 #struct_mirror = [Layer(si('120nm'), TiO2, geometry=[{'type': 'rectangle', 'mat': Ag, 'center': (x/2, x/2),
                                                    #  'halfwidths': (210,210), 'angle': 0}])]
 
-grating =  [Layer(si('120nm'), Si, geometry=[{'type': 'rectangle', 'mat': Ag, 'center': (x/2, x/2),
+grating =  [Layer(si('120nm'), Si, geometry=[{'type': 'rectangle', 'mat': Air, 'center': (x/2, x/2),
                                                      'halfwidths': (np.sqrt(2*(500**2))/2, np.sqrt(2*(500**2))/2), 'angle': 45}])]
 # NP_layer=[Layer(si('50nm'), Ag)]
 
@@ -43,7 +42,7 @@ grating =  [Layer(si('120nm'), Si, geometry=[{'type': 'rectangle', 'mat': Ag, 'c
 
 solar_cell = SolarCell(grating)
 
-orders = 70
+orders = 20
 wavelengths = np.linspace(600, 1100, 4)*1e-9
 
 options = {'nm_spacing': 0.5,
@@ -58,7 +57,7 @@ options = {'nm_spacing': 0.5,
            'phi_symmetry': np.pi/2,
            }
 
-output = calculate_rat_rcwa(solar_cell, size, orders, options, incidence=Si, substrate=Air, only_incidence_angle=False,
+output = rcwa(solar_cell, size, orders, options, incidence=Air, substrate=Si, only_incidence_angle=False,
                        front_or_rear='front', surf_name='OPTOS')
 
 #plt.plot(wavelengths, output['A_layer'][:, 0, 0])
@@ -67,7 +66,7 @@ output = calculate_rat_rcwa(solar_cell, size, orders, options, incidence=Si, sub
 #plt.plot(wavelengths, output['A_layer'][:, 0, 3])
 plt.plot(wavelengths, output['R'][:,0])
 plt.plot(wavelengths, output['T'][:,0])
-plt.legend(['ARC1', 'ARC2', 'GaAs', 'struct mirror', 'R', 'T'])
+plt.legend(['R', 'T'])
 plt.show()
 #
 # #plt.figure()
@@ -86,21 +85,40 @@ plt.show()
 # print(output['T'][7])
 # print(np.sum(output['T_pfbo'][7]))
 
-theta_intv, phi_intv, angle_vector = make_angle_vector(options['n_theta_bins'], options['phi_symmetry'], options['c_azimuth'])
+from sparse import load_npz
+from config import results_path
+import os
 
-R_mat = output['R_mat'][3]
-T_mat = output['T_mat'][3]
+sprs = load_npz(os.path.join(results_path, options['project_name'], 'crossed_grating' + 'frontRT.npz'))
 
-fullmat = np.hstack((R_mat, T_mat))
+full = sprs[15].todense()
 
-sum_mat, R, T = theta_summary(R_mat, angle_vector)
+theta_intv, phi_intv, angle_vector = make_angle_vector(100, options['phi_symmetry'], options['c_azimuth'])
 
-sum_mat = xr.DataArray(sum_mat, dims=['sin_out', 'sin_in'], coords={'sin_out': np.linspace(0,1,options['n_theta_bins']),
-                       'sin_in': np.linspace(0,1,options['n_theta_bins'])})
+#R_mat = output['full_mat'][3].todense()
+
+RT_sum, R, T = theta_summary(full, angle_vector)
+
+sum_mat = xr.DataArray(RT_sum[0:100], dims=[r'$\sin(\theta_{out})$', r'$\sin(\theta_{in})$'], coords={r'$\sin(\theta_{out})$': np.linspace(0,1,100),
+                                                                            r'$\sin(\theta_{in})$': np.linspace(0,1,100)})
+
+import matplotlib as mpl
+import seaborn as sns
+palhf = sns.cubehelix_palette(256, start=.5, rot=-.9)
+palhf.reverse()
+seamap = mpl.colors.ListedColormap(palhf)
+fig = plt.figure()
+ax = plt.subplot(111)
+ax = sum_mat.plot.imshow(ax=ax, cmap=seamap)
+#ax = plt.subplot(212)
+
+
+fig.savefig('matrix_grating.png', bbox_inches='tight', format='png')
+
+sum_mat = xr.DataArray(RT_sum[100:], dims=[r'$\sin(\theta_{out})$', r'$\sin(\theta_{in})$'], coords={r'$\sin(\theta_{out})$': np.linspace(0,1,100),
+                                                                            r'$\sin(\theta_{in})$': np.linspace(0,1,100)})
 
 fig = plt.figure()
 ax = plt.subplot(111)
 sum_mat.plot.imshow(ax=ax, vmax=5)
-
-
 
