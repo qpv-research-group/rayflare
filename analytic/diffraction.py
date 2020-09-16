da142bfb3fcfd14b3aa8b38c786e1ceca0ef266d
Phoebe Pearce <phoebe@Phoebes-iMac.local>
2020-09-16 21:49:01 +0100

    add docs folder

--- analytic/diffraction.py
+++ analytic/diffraction.py
@@ -12,11 +12,11 @@ def get_reciprocal_lattice(size, orders):
 
     f_mat = S.GetReciprocalLattice()
 
-    return f_mat
+    return 2*np.pi*np.array(f_mat)
 
 def get_order_directions(wl, size, m_max, incidence, transmission, theta, phi, phi_sym=np.pi/2):
     # wl in nm
-    rl = get_reciprocal_lattice(size, 9)
+    rl = get_reciprocal_lattice(size, 9)/(2*np.pi)
 
     fi_x = np.real((incidence.n(wl * 1e-9) / wl) * np.sin(theta) *
                    np.sin(phi))
@@ -46,7 +46,55 @@ def get_order_directions(wl, size, m_max, incidence, transmission, theta, phi, p
     theta_r = np.real(np.arccos(fr_z / np.sqrt(fr_x ** 2 + fr_y ** 2 + fr_z ** 2)))
     theta_t = np.pi - np.real(np.arccos(ft_z / np.sqrt(fr_x ** 2 + fr_y ** 2 + ft_z ** 2)))
 
-    return {'order_index': [xv, yv], 'phi': phi_rt, 'theta_r': theta_r, 'theta_t': theta_t, 'fr_x': fr_x, 'fr_y': fr_y}
+    kxy = np.sqrt(np.sum((xv.flatten()[:, None] * 2*np.pi*rl[0] + yv.flatten()[:, None] * 2*np.pi*rl[1]) ** 2, 1))
+
+    return {'order_index': [xv, yv], 'phi': phi_rt, 'theta_r': theta_r,
+            'theta_t': theta_t, 'fr_x': fr_x, 'fr_y': fr_y, 'k_xy': kxy}
+
+
+def group_diffraction_orders(size, basis_set, per_order=None):
+    lv = np.array(size).T
+    a1 = lv[:, 0]
+    a2 = lv[:, 1]
+
+    # f_mat = RAT['reciprocal']
+    # fg_1x = f_mat[0][0]
+    # fg_1y = f_mat[0][1]
+    # fg_2x = f_mat[1][0]
+    # fg_2y = f_mat[1][1]
+    #
+    # fr_x = basis_set[:, 0] * fg_1x + basis_set[:, 1] * fg_2x
+    # fr_y = basis_set[:, 0] * fg_1y + basis_set[:, 1] * fg_2y
+
+    # reciprocal lattice vector
+    R = np.array([[0, -1], [1, 0]])
+
+    b1 = 2 * np.pi * np.dot(R, a2) / (np.dot(a1, np.dot(R, a2)))
+    b2 = 2 * np.pi * np.dot(R, a1) / (np.dot(a2, np.dot(R, a1)))
+
+
+    kxy = np.sqrt(np.sum((np.array(basis_set)[:, 0][:, None] * b1 + np.array(basis_set)[:, 1][:, None] * b2) ** 2, 1))
+
+    unique_kxy = np.unique(np.round(kxy, 10),
+                         return_index=True, return_counts=True, return_inverse=True)
+
+
+
+    if per_order is not None:
+
+        indices = unique_kxy[2]
+        unique_power = np.zeros((per_order.shape[0], len(unique_kxy[0])))
+        indiv_powers = [[] for _ in range(len(unique_kxy[0]))]
+        for j1, ind in enumerate(indices):
+            unique_power[:, ind] = unique_power[:, ind] + per_order[:, j1]
+            indiv_powers[ind].append(per_order[0, j1])
+
+        degen = [len(np.unique(np.round(x, 10))) for x in indiv_powers]
+
+        return {'k_xy': unique_kxy[0], 'reps': unique_kxy[3], 'per_order': unique_power, 'degeneracy': degen}
+
+    else:
+        return {'k_xy': unique_kxy[0], 'reps': unique_kxy[3]}
 
 
 def get_order_directions_nk(wl, size, m_max, incidence, transmission, theta, phi, phi_sym=np.pi/2):
