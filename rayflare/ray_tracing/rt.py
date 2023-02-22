:bug: fix very stupid mistake calculating outgoing theta in RT; affects absorption calculation

rayflare/ray_tracing/rt.py
@@ -1046,7 +1046,7 @@ def single_interface_check(r_a, d, ni, nj, tri, Lx, Ly, side, z_cov, pol, n_inte
                     # ray keeps missing, probably because it's travelling (almost) exactly perpendicular to surface.
                     # assume it is reflected back into layer it came from
                     d[2] = -d[2]
-                    o_t = np.real(acos(d[2] / (np.linalg.norm(d) ** 2)))
+                    o_t = np.real(acos(d[2] / np.linalg.norm(d)))
                     o_p = np.real(atan2(d[1], d[0]))
                     return 0, o_t, o_p, r_a, d, 0, n_interactions, side
 
@@ -1066,7 +1066,7 @@ def single_interface_check(r_a, d, ni, nj, tri, Lx, Ly, side, z_cov, pol, n_inte
 
             else:
 
-                o_t = np.real(acos(d[2] / (np.linalg.norm(d) ** 2)))
+                o_t = np.real(acos(d[2] / np.linalg.norm(d)))
                 o_p = np.real(atan2(d[1], d[0]))
 
                 if np.sign(d0[2]) == np.sign(d[2]):
@@ -1145,7 +1145,7 @@ def single_cell_check(r_a, d, ni, nj, tri, Lx, Ly, side, z_cov, pol, n_interacti
 
             n_misses += 1
 
-            o_t = np.real(acos(d[2] / (np.linalg.norm(d) ** 2)))
+            o_t = np.real(acos(d[2] / np.linalg.norm(d)))
             o_p = np.real(atan2(d[1], d[0]))
 
             if np.sign(d0[2]) == np.sign(d[2]):