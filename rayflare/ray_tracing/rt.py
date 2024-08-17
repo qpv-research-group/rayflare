think issue with scaling absorption profiles/indexing lookuptable is fixed

rayflare/ray_tracing/rt.py
@@ -621,9 +621,10 @@ def calculate_interface_profiles(
     depth_spacing,
 ):
 
-    def profile_per_layer(x, z, offset, side):
-        layer_index = x.coords["layer"].item(0) - 1
+    def profile_per_layer(xx, z, offset, side):
 
+        layer_index = xx.coords["layer"].item(0) - 1
+        x = xx.squeeze(dim="layer")
         part1 = x[:, 0] * np.exp(x[:, 4] * z[layer_index])
         part2 = x[:, 1] * np.exp(-x[:, 4] * z[layer_index])
         part3 = (x[:, 2] + 1j * x[:, 3]) * np.exp(1j * x[:, 5] * z[layer_index])
@@ -642,6 +643,7 @@ def calculate_interface_profiles(
         )
         return by_layer
 
+    # lookuptable should only have s and p entries (in that order) at this point
     th_array = np.abs(local_thetas_i)
     front_incidence = np.where(directions_i == 1)[0]
     rear_incidence = np.where(directions_i == -1)[0]
@@ -699,15 +701,19 @@ def calculate_interface_profiles(
 
             scale_res = local_pols_dir[i1][:,None, None]*s_params_pol * scale_factor[:, None, None]
 
-            params_pol = xr.concat((scale_res, c_params_pol), dim="coeff")
+            if np.sum(scale_res) > 0:
+                params_pol = xr.concat((scale_res, c_params_pol), dim="coeff")
 
-            ans_pol = (
-                params_pol.groupby("angle", squeeze=False)
-                .map(profile_per_angle, z=z_list, offset=offsets, side=1)
-                .drop_vars("coeff")
-            )
+                ans_pol = (
+                    params_pol.groupby("angle", squeeze=False)
+                    .map(profile_per_angle, z=z_list, offset=offsets, side=1)
+                    .drop_vars("coeff")
+                )
 
-            ans_list.append(ans_pol.reduce(np.sum, ["angle"]).fillna(0))
+                ans_list.append(ans_pol.reduce(np.sum, ["angle"]).fillna(0))
+
+            else:
+                ans_list.append(0)
 
         profile_front = ans_list[0] + ans_list[1]
 
@@ -753,15 +759,19 @@ def calculate_interface_profiles(
             scale_res = local_pols_dir[i1][:, None, None] * s_params_pol * scale_factor[:, None,
                                                                            None]
 
-            params_pol = xr.concat((scale_res, c_params_pol), dim="coeff")
+            if np.sum(scale_res) > 0:
+                params_pol = xr.concat((scale_res, c_params_pol), dim="coeff")
 
-            ans_pol = (
-                params_pol.groupby("angle", squeeze=False)
-                .map(profile_per_angle, z=z_list, offset=offsets, side=1)
-                .drop_vars("coeff")
-            )
+                ans_pol = (
+                    params_pol.groupby("angle", squeeze=False)
+                    .map(profile_per_angle, z=z_list, offset=offsets, side=-1)
+                    .drop_vars("coeff")
+                )
 
-            ans_list.append(ans_pol.reduce(np.sum, ["angle"]).fillna(0))
+                ans_list.append(ans_pol.reduce(np.sum, ["angle"]).fillna(0))
+
+            else:
+                ans_list.append(0)
 
         profile_back = ans_list[0] + ans_list[1]
 
@@ -792,6 +802,150 @@ def calculate_interface_profiles(
 
     else:
         return []
+    #
+    # def profile_per_layer(x, z, offset, side):
+    #     layer_index = x.coords["layer"].item(0) - 1
+    #
+    #     part1 = x[:, 0] * np.exp(x[:, 4] * z[layer_index])
+    #     part2 = x[:, 1] * np.exp(-x[:, 4] * z[layer_index])
+    #     part3 = (x[:, 2] + 1j * x[:, 3]) * np.exp(1j * x[:, 5] * z[layer_index])
+    #     part4 = (x[:, 2] - 1j * x[:, 3]) * np.exp(-1j * x[:, 5] * z[layer_index])
+    #     result = np.real(part1 + part2 + part3 + part4)
+    #
+    #     if side == -1:
+    #         result = np.flip(result, 1)
+    #     return result.reduce(np.sum, axis=0).assign_coords(
+    #         dim_0=z[layer_index] + offset[layer_index]
+    #     )
+    #
+    # def profile_per_angle(x, z, offset, side):
+    #     by_layer = x.groupby("layer").map(
+    #         profile_per_layer, z=z, offset=offset, side=side
+    #     )
+    #     return by_layer
+    #
+    # th_array = np.abs(local_thetas_i)
+    # front_incidence = np.where(directions_i == 1)[0]
+    # rear_incidence = np.where(directions_i == -1)[0]
+    #
+    # # need to scale absorption profile for each ray depending on
+    # # how much intensity was left in it when that ray was absorbed (this is done for total absorption inside
+    # # single_ray_stack)
+    #
+    # pol = 's'
+    #
+    # if len(front_incidence) > 0:
+    #
+    #     A_lookup_front = lookuptable.Alayer.loc[
+    #         dict(side=1, pol=pol, layer=prof_layer_list_i)
+    #     ].interp(angle=th_array[front_incidence])
+    #     data_front = data_prof_layers[front_incidence]
+    #
+    #     ## CHECK! ##
+    #     non_zero = xr.where(A_lookup_front > 1e-10, A_lookup_front, np.nan)
+    #
+    #     scale_factor = (
+    #         np.divide(data_front, non_zero).mean(dim="layer", skipna=True).data
+    #     )  # can get slight differences in values between layers due to lookuptable resolution
+    #
+    #     # layers because lookuptable angles are not exactly the same as the angles of the rays when absorbed. Take mean.
+    #     # TODO: check what happens when one of these is zero or almost zero?
+    #
+    #     # note that if a ray is absorbed in the interface on the first pass, the absorption per layer
+    #     # recorded in A_interfaces will be LARGER than the A from the lookuptable because the lookuptable
+    #     # includes front surface reflection, and by definition if the ray was absorbed it was not reflected
+    #     # so the sum of the absorption per layer recorded in A_interfaces is 1 while the sum of the absorption in the
+    #     # lookuptable is 1 - R - T.
+    #
+    #     params_front = lookuptable.Aprof.loc[
+    #         dict(side=1, pol=pol, layer=prof_layer_list_i)
+    #     ].interp(angle=th_array[front_incidence])
+    #
+    #     import matplotlib.pyplot as plt
+    #     plt.hist(th_array[front_incidence])
+    #     plt.show()
+    #     s_params = params_front.loc[
+    #         dict(coeff=["A1", "A2", "A3_r", "A3_i"])
+    #     ]  # have to scale these to make sure integrated absorption is correct
+    #     c_params = params_front.loc[
+    #         dict(coeff=["a1", "a3"])
+    #     ]  # these should not be scaled
+    #
+    #     scale_res = s_params * scale_factor[:, None, None]
+    #
+    #     params_front = xr.concat((scale_res, c_params), dim="coeff")
+    #
+    #     ans_front = (
+    #         params_front.groupby("angle", squeeze=False)
+    #         .map(profile_per_angle, z=z_list, offset=offsets, side=1)
+    #         .drop_vars("coeff")
+    #     )
+    #
+    #     profile_front = ans_front.reduce(np.sum, ["angle"]).fillna(0)
+    #
+    # else:
+    #     profile_front = 0
+    #
+    # if len(rear_incidence) > 0:
+    #
+    #     A_lookup_back = lookuptable.Alayer.loc[
+    #         dict(side=-1, pol=pol, layer=prof_layer_list_i)
+    #     ].interp(angle=th_array[rear_incidence])
+    #
+    #     data_back = data_prof_layers[rear_incidence]
+    #
+    #     non_zero = xr.where(A_lookup_back > 1e-10, A_lookup_back, np.nan)
+    #
+    #     scale_factor = (
+    #         np.divide(data_back, non_zero).mean(dim="layer", skipna=True).data
+    #     )  # can get slight differences in values between layers
+    #
+    #     params_back = lookuptable.Aprof.loc[
+    #         dict(side=-1, pol=pol, layer=prof_layer_list_i)
+    #     ].interp(angle=th_array[rear_incidence])
+    #
+    #     s_params = params_back.loc[
+    #         dict(coeff=["A1", "A2", "A3_r", "A3_i"])
+    #     ]  # have to scale these to make sure integrated absorption is correct
+    #     c_params = params_back.loc[
+    #         dict(coeff=["a1", "a3"])
+    #     ]  # these should not be scaled
+    #
+    #     scale_res = s_params * scale_factor[:, None, None]
+    #
+    #     params_back = xr.concat((scale_res, c_params), dim="coeff")
+    #
+    #     ans_back = (
+    #         params_back.groupby("angle", squeeze=False)
+    #         .map(profile_per_angle, z=z_list, offset=offsets, side=-1)
+    #         .drop_vars("coeff")
+    #     )
+    #
+    #     profile_back = ans_back.reduce(np.sum, ["angle"]).fillna(0)
+    #
+    # else:
+    #
+    #     profile_back = 0
+    #
+    # profile = profile_front + profile_back
+    #
+    # integrated_profile = np.sum(profile.reduce(np.trapz, dim="dim_0", dx=depth_spacing))
+    #
+    # A_corr = np.sum(A_in_prof_layers)
+    #
+    # scale_profile = np.real(
+    #     np.divide(
+    #         A_corr,
+    #         integrated_profile.data,
+    #         where=integrated_profile.data > 0,
+    #         out=np.zeros_like(A_corr),
+    #     )
+    # )
+    #
+    # interface_profile = scale_profile * profile.reduce(np.sum, dim="layer")
+    #
+    # return interface_profile.data
+
 
 def make_rt_args(existing_rays, xs, ys, n_reps):
     """Make arguments for single_ray_stack with existing rays from analytical calculation."""
@@ -1442,7 +1596,8 @@ def make_tmm_args(arg_list):
 
             structpath = arg_list[2]
             surf_name = arg_list[3][i1] + "int_{}".format(i1)
-            lookuptable = xr.open_dataset(os.path.join(structpath, surf_name + ".nc")).loc[dict(wl=arg_list[-1]*1e9)].sel(pol=['s', 'p']).load()
+            lookuptable = xr.open_dataset(os.path.join(structpath, surf_name + ".nc")).loc[dict(wl=arg_list[-1]*1e9)].load()
+            lookuptable = lookuptable.sel(pol=['s', 'p'])
             additional_tmm_args.append(
                 {"Fr_or_TMM": 1, "lookuptable": lookuptable}
             )