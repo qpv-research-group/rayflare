92add757468b327aeb689122e56734e06b9ffe04
Phoebe Pearce <p.pearce@unsw.edu.au>
2024-08-23 13:26:20 +1000

    remove -ve values from out_mat

--- rayflare/ray_tracing/rt_matrix.py
+++ rayflare/ray_tracing/rt_matrix.py
@@ -453,10 +453,13 @@ def RT_wl(
     out_mat[np.isnan(out_mat)] = 0
     A_mat[np.isnan(A_mat)] = 0
 
-    if np.any(out_mat < 0):
-        print(out_mat)
-        print('min in outmat:', np.min(out_mat))
-        raise ValueError(f"Negative values in out_mat, {np.min(out_mat)}")
+    out_mat[out_mat < 0] = 0
+    A_mat[A_mat < 0] = 0
+
+    # if np.any(out_mat < 0):
+    #     print(out_mat)
+    #     print('min in outmat:', np.min(out_mat))
+    #     raise ValueError(f"Negative values in out_mat, {np.min(out_mat)}")
 
     out_mat = COO.from_numpy(out_mat)  # sparse matrix
     A_mat = COO.from_numpy(A_mat)
